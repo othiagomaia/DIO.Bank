d4e5f9df14ad9dbd5aacf6ef2838e2805b195011
Thiago Maia <othiagomaia@live.com>
2021-08-14 20:18:49 -0400

    Aula 2

--- DIO.Bank/Classes/Conta.cs
+++ DIO.Bank/Classes/Conta.cs
@@ -18,6 +18,43 @@ namespace DIO.Bank
             this.Credito = credito;
             this.Nome = nome;
         }
+
+        public bool Sacar(double valorSaque)
+        {
+            if (Saldo - valorSaque <(Credito *-1))
+            {
+                Console.WriteLine("Saldo insuficiente!");
+                return false;
+            }
+
+            Saldo -= valorSaque;
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", Nome, Saldo);
+            return true;
+        }
+
+
+        public void Depositar(double valorDeposito)
+        {
+            Saldo += valorDeposito;
+
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", Nome, Saldo);
+        }
+
+        public void Transferir(double valorTransferencia, Conta contaDestino)
+        {
+            if (Sacar(valorTransferencia))
+                contaDestino.Depositar(valorTransferencia);
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "TipoConta " + TipoConta + " | ";
+            retorno += "Nome " + Nome + " | ";
+            retorno += "Saldo " + Saldo + " | ";
+            retorno += "Credito " + Credito;
+            return retorno;
+        }
     }
 
     public enum TipoConta
