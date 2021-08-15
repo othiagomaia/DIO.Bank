d8c7a169bec2b29aeb486e3c2264ab4b1ae99db6
Thiago Maia <othiagomaia@live.com>
2021-08-14 20:00:58 -0400

    Aula 1

---
+++ DIO.Bank/Classes/Conta.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DIO.Bank
+{
+    public class Conta
+    {
+        private TipoConta TipoConta { get; set; }
+        private double Saldo { get; set; }
+        private double Credito { get; set; }
+        private string Nome { get; set; }
+
+        public Conta(TipoConta tipoConta, double saldo, double credito, string nome)
+        {
+            this.TipoConta = tipoConta;
+            this.Saldo = saldo;
+            this.Credito = credito;
+            this.Nome = nome;
+        }
+    }
+
+    public enum TipoConta
+    {
+        PessoaFisica = 1,
+        PessoaJuridica
+    }
+}
